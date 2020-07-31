bf4ce4e29cb8b8dd4cf4cec2db773bd438815e88
Andrew Xu <andwxu@gmail.com>
2020-07-31 18:19:24 -0400

    added new line

--- src/Hi.js
+++ src/Hi.js
@@ -26,12 +26,19 @@ export default function Hi() {
         delay: 2000,
     });
 
+    const props5 = useSpring({
+        opacity: 1,
+        from: { opacity: 0 },
+        delay: 2500,
+    });
+
     return (
         <div>
             <animated.h1 style={props}>Hi.</animated.h1>
             <animated.h2 style={props2}> I'm</animated.h2>
             <animated.h3 style={props3}>Andrew Xu</animated.h3>
             <animated.h4 style={props4}>•</animated.h4>
+            <animated.h5 style={props5}>CS Student at Georgia Tech</animated.h5>
         </div>
     )
 }
--- src/Highlight.js
+++ src/Highlight.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
 export default function Highlight() {
-    const [state, toggle] = useState(true);
     const props = useSpring({
-        from: {width: '0px', background: state ? 'black' : 'lightblue'},
-        to: {width: '320px', background: state ? 'lightblue' : 'black'},
+        from: { width: '0px', background: 'lightblue' },
+        to: { width: '320px' },
         delay: 1500,
     });
 
+    const props2 = useSpring({
+        from: { width: '0px', background: 'lightyellow', marginLeft: '145px'},
+        to: { width: '140px' },
+        delay: 3000,
+    });
+
     return (
-        <div style={wrapper} onClick={() => toggle(!state)}>
+        <div style={wrapper}>
             <animated.div style={props}>
                 <div style={style}></div>
+            </animated.div>
+            <animated.div style={props2}>
+                <div style={style2}></div>
             </animated.div>
         </div>
     )
@@ -20,6 +28,12 @@ export default function Highlight() {
 
 const style = {
     height: '90px',
+    textAlign: 'center',
+    verticalAlign: 'middle',
+}
+
+const style2 = {
+    height: '30px',
     textAlign: 'center',
     verticalAlign: 'middle',
 }
