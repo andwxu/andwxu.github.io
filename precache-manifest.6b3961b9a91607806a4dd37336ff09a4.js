3a4e96e6af48d9366f5d497f20524b1f1b7a765b
Andrew Xu <andwxu@gmail.com>
2020-07-31 17:42:17 -0400

    Updates

---
+++ precache-manifest.6b3961b9a91607806a4dd37336ff09a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1162922dc26a537c062d149a7f9b741",
+    "url": "/andwxu.github.io/index.html"
+  },
+  {
+    "revision": "862c707abbf9b3325189",
+    "url": "/andwxu.github.io/static/css/main.66955454.chunk.css"
+  },
+  {
+    "revision": "dd8d6a7fc6537bb4a460",
+    "url": "/andwxu.github.io/static/js/2.cf74ab95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andwxu.github.io/static/js/2.cf74ab95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "862c707abbf9b3325189",
+    "url": "/andwxu.github.io/static/js/main.8b04c5c5.chunk.js"
+  },
+  {
+    "revision": "1113b5649de53df7c1b5",
+    "url": "/andwxu.github.io/static/js/runtime-main.1468a81a.js"
+  }
+]);
